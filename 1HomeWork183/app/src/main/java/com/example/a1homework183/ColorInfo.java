b8d2a91ade3d849a4cc1af6f7f6d65272e8b7655
chaseothan <52476148+chaseothan@users.noreply.github.com>
2023-09-25 15:27:54 -0400

    xczxcz
    zxcxc

--- 1HomeWork183/app/src/main/java/com/example/a1homework183/ColorInfo.java
+++ 1HomeWork183/app/src/main/java/com/example/a1homework183/ColorInfo.java
@@ -3,4 +3,61 @@ package com.example.a1homework183;
 public class ColorInfo
 {
 
+    public int getRed() {
+        return red;
+    }
+
+    public void setRed(int red)
+    {
+        this.red = red;
+    }
+
+    public int getGreen()
+    {
+        return green;
+    }
+
+    public void setGreen(int green)
+    {
+        this.green = green;
+    }
+
+    public int getBlue()
+    {
+        return blue;
+    }
+
+    public void setBlue(int blue)
+    {
+        this.blue = blue;
+    }
+
+    private int red;
+
+    private int green;
+
+    private int blue;
+
+    public ColorInfo(int r,int g, int b)
+    {
+
+        red = r;
+        green = g;
+        blue = b;
+
+    }
+    public ColorInfo()
+
+    {
+
+    }
+
+    public String getHexDecimal() {
+        return hexDecimal;
+    }
+
+    private String hexDecimal;
+
+
+
 }
